6a98c337ed59d6989bd7d73f39276505b8d31b7a
Zoltan Nagy <abesto@abesto.net>
2016-01-26 10:41:52 +0100

    Let it be so.

---
+++ 1/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# vim: ts=4
+def multiply(a, b):
+	result = 0
+    for n in range(b):
+        # wheee a comment
+        result += a
+    return result
+
+print multiply(3, 10)
